89c1c9ac8682397c88b9e78922e42b442cd3f575
nguyenledongmanh <nguyenle.dongmanh@gmail.com>
2022-03-24 19:18:57 +0700

    first lib

---
+++ CreateLibrary/app/mylibrary/src/main/java/com/example/mylibrary/CustomMinMax.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+public class CustomMinMax {
+    public int min(int a, int b) {
+        return a > b ? b : a;
+    }
+
+    public int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
